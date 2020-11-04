55d77334031a7c38a3a1aef3ba5bd4bfc7a676a1
芭无寓 度麻素 <bangdompret@gmail.com>
2020-11-04 13:51:16 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,119 @@
+require('dotenv').config();
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const alive = require("./alive.js")
+
+const { GiveawaysManager } = require("discord-giveaways");
+const db = require("quick.db");
+if(!db.get("giveaways")) db.set("giveaways", []);
+
+const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
+
+    // This function is called when the manager needs to get all the giveaway stored in the database.
+    async getAllGiveaways(){
+        // Get all the giveaway in the database
+        return db.get("giveaways");
+    }
+
+    // This function is called when a giveaway needs to be saved in the database (when a giveaway is created or when a giveaway is edited).
+    async saveGiveaway(messageID, giveawayData){
+        // Add the new one
+        db.push("giveaways", giveawayData);
+        // Don't forget to return something!
+        return true;
+    }
+
+    async editGiveaway(messageID, giveawayData){
+        // Gets all the current giveaways
+        const giveaways = db.get("giveaways");
+        // Remove the old giveaway from the current giveaways ID
+        const newGiveawaysArray = giveaways.filter((giveaway) => giveaway.messageID !== messageID);
+        // Push the new giveaway to the array
+        newGiveawaysArray.push(giveawayData);
+        // Save the updated array
+        db.set("giveaways", newGiveawaysArray);
+        // Don't forget to return something!
+        return true;
+    }
+
+    // This function is called when a giveaway needs to be deleted from the database.
+    async deleteGiveaway(messageID){
+        // Remove the giveaway from the array
+        const newGiveawaysArray = db.get("giveaways").filter((giveaway) => giveaway.messageID !== messageID);
+        // Save the updated array
+        db.set("giveaways", newGiveawaysArray);
+        // Don't forget to return something!
+        return true;
+    }
+
+};
+
+// Create a new instance of your new class
+const manager = new GiveawayManagerWithOwnDatabase(client, {
+    storage: false,
+    updateCountdownEvery: 5000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: [ "MANAGE_MESSAGES", "ADMINISTRATOR" ],
+        embedColor: "#FF0000",
+        reaction: "ðŸŽ‰"
+    }
+});
+client.giveawaysManager = manager;
+// We now have a client.giveawaysManager property to manage our giveaways!
+
+client.giveawaysManager.on("giveawayReactionAdded", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} entered giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
+});
+
+client.giveawaysManager.on("giveawayReactionRemoved", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} unreact to giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
+});
+
+
+const config = require("./config.json"),
+fs = require("fs"),
+util = require("util"),
+readdir = util.promisify(fs.readdir),
+mongoose = require("mongoose");
+
+
+client.logger = require("./modules/Logger.js");
+client.errors = require("./modules/Embeds.js");
+client.tools = require("./modules/Tools.js");
+client.data = require("./modules/MongoDB.js");
+
+client.events = new Discord.Collection();
+client.commands = new Discord.Collection();
+client.categories = [];
+async function init(){
+
+const eventFiles = fs.readdirSync('./events/').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    const eventName = file.split(".")[0];
+    client.logger.event(`Loading Event - ${eventName}`);
+    client.on(eventName, event.bind(null, client));
+}
+
+let folders = await readdir("./commands/");
+folders.forEach(direct =>{
+  const commandFiles = fs.readdirSync('./commands/' + direct + "/").filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+      const command = require(`./commands/${direct}/${file}`);
+      client.commands.set(command.name, command);
+  }
+  })
+  
+  mongoose.connect(process.env.mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+      client.logger.log("Connected to the Mongodb database.", "log");
+  }).catch((err) => {
+      client.logger.log("Unable to connect to the Mongodb database. Error:"+err, "error");
+  });
+
+
+}
+
+init();
+client.login(process.env.TOKEN)
