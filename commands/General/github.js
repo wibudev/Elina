28938a6aa8c7d2f33d8421e3342347bb18e1f52e
芭无寓 度麻素 <bangdompret@gmail.com>
2020-11-06 18:41:21 +0700

    Create github.js

---
+++ commands/General/github.js
@@ -0,0 +1,52 @@
+const { Discord, discord } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const moment = require("moment");
+const fetch = require("node-fetch")
+
+module.exports = {
+name: "github",
+    description: "Github Account",
+    usage: "{prefix}github username",
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    memberPermissions: [],
+    botPermissions: ["SEND_MESSAGES","EMBED_LINKS"],
+    nsfw: false,
+    cooldown: 5000,
+    ownerOnly: false,
+     async execute (client, message, args){
+
+       try {
+
+  if (!args[0]) return message.channel.send(`Please Give Me A Username!`)
+    
+  fetch(`https://api.github.com/users/${args.join('-')}`)
+    .then(res => res.json()).then(body => {
+      if(body.message) return message.channel.send(`User Not Found | Please Give Me A Valid Username!`);
+    let { login, avatar_url, name, id, html_url, public_repos, followers, following, location, created_at, bio } = body;
+
+            const embed = new MessageEmbed()
+            .setAuthor(`${login} Information!`, avatar_url)
+            .setColor(`#211F1F`)
+            .setThumbnail(`${avatar_url}`)
+            .addField(`Username`, `${login}`)
+            .addField(`ID`, `${id}`)
+            .addField(`Bio`, `${bio || "No Bio"}`)
+            .addField(`Public Repositories`, `${public_repos || "None"}`, true)
+            .addField(`Followers`, `${followers}`, true)
+            .addField(`Following`, `${following}`, true)
+            .addField(`Location`, `${location || "No Location"}`)
+            .addField(`Account Created`, moment.utc(created_at).format("dddd, MMMM, Do YYYY"))
+            .setFooter(`Thx For Using Me! ${message.author.username}`)
+
+            message.channel.send(embed)
+
+    })
+
+        } catch (error) {
+            console.log(`[Commands] [github] Getting Error In github Command :\n`, error);
+            return message.channel.send(`Something Went Wrong Try Again Later!`)
+        }
+    }
+};
