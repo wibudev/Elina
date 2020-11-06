0e2aa88e3f29601fd966c6958e9de92562fca48d
芭无寓 度麻素 <bangdompret@gmail.com>
2020-11-06 18:23:07 +0700

    Create Member.js

---
+++ base/Member.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+module.exports = mongoose.model("Member", new mongoose.Schema({
+    id: { type: String }, 
+    guildID: { type: String }, 
+    registeredAt: { type: Number, default: Date.now() }, 
+
+}));
