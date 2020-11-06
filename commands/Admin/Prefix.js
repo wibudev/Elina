64e5375408c16c91b0090097f0ae6157a03cba37
芭无寓 度麻素 <bangdompret@gmail.com>
2020-11-06 18:52:54 +0700

    Create Prefix.js

---
+++ commands/Admin/Prefix.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+
+module.exports = {
+      name: "prefix",
+      description: "Set the prefix for the guild.",
+      usage: "`{prefix}prefix {prefix}`",
+      enabled: true,
+      guildOnly: true,
+      aliases: [],
+      memberPermissions: [],
+      botPermissions: [],
+      nsfw: false,
+      cooldown: 5000,
+      ownerOnly: false,
+
+    async execute(client, message, args, data) {
+      if (
+			!message.member.hasPermission('ADMINISTRATOR', {
+				checkAdmin: true,
+				checkOwner: true
+			})
+		)
+			return message.channel.send(
+				"**I'm sorry, you don't have the permission to use this.**"
+			);
+      let prefix = args[0];
+      if(!prefix){
+        return message.channel.send("This guilds prefix is: " + data.guild.prefix);
+      }
+
+      if(prefix){
+        data.guild.prefix = prefix;
+        await data.guild.save();
+        return message.channel.send("Guild prefix has been changed to " + prefix)
+      }
+
+
+    },
+};
+
