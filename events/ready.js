d1067370b1e36622fefffcd35edb5e8ed80bca23
芭无寓 度麻素 <bangdompret@gmail.com>
2020-11-06 18:25:40 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,22 @@
+module.exports = async (client) => {
+const activity_list = 
+[ 
+  [`JavaScript`, "PLAYING", "idle"],
+  ["bacotan-wibu.com", "LISTENING","idle"]
+  ];
+try {
+        setInterval(() => {
+		const index = Math.floor(Math.random() * activity_list.length);
+		client.user.setPresence({
+            activity:  {
+                            name: activity_list[index][0], 
+                             type: activity_list[index][1],
+         },
+          status: activity_list[index][2],
+    })
+	}, 7000);
+    } catch (e) {
+        console.log(e);
+   }
+}
 
