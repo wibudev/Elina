3c009fa8c7861ad32dcdc36c7173846cf91ae18d
芭无寓 度麻素 <bangdompret@gmail.com>
2020-11-19 06:16:56 +0700

    *

--- commands/Admin/voice.js
+++ commands/Admin/voice.js
@@ -1,1 +1,34 @@
+const Discord = require("discord.js")
+const config = require("../../config.json")
 
+module.exports = {
+    name: "createtext",
+    description: "Create Voice Channel",
+    usage: "createtext",
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    memberPermissions: [],
+    botPermissions: [],
+    nsfw: false,
+    cooldown: 5000,
+    ownerOnly: false,
+
+async execute(client, message, args, data) {
+	if (
+		!message.member.hasPermission('ADMINISTRATOR', {
+			checkAdmin: true,
+			checkOwner: true
+		})
+	)
+		return message.channel.send(
+			"**You don't have the permission to do this**"
+		);
+const name = message.content.replace(data.guild.prefix+'createtext', '');
+	message.guild.channels
+		.create(name, {
+			type: 'voice'
+		})
+		.then(channel => {
+	});
+}}
